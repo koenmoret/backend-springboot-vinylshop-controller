a80af6092e60332c749250880cf4f10f7ad7cf2f
koenmoret <koen@moret.info>
2025-11-23 12:19:51 +0100

    Initial commit

---
+++ src/main/java/com/vinylwebshop/vinylcollection/VinylcollectionApplication.java
@@ -0,0 +1,13 @@
+package com.vinylwebshop.vinylcollection;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VinylcollectionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VinylcollectionApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/vinylwebshop/vinylcollection/VinylcollectionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vinylwebshop.vinylcollection;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VinylcollectionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
